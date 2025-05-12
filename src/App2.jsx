6607aa181ddfd1ad53b0b79bf7fa93fb945ff0bd
Luiz Filipe <101185482+lzribeiro@users.noreply.github.com>
2025-05-12 19:20:22 -0300

    Add files via upload

---
+++ src/App2.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const App2 = () => {
+  return <div>App2</div>;
+};
+
+export default App2;
+// Arrow Function
+//const App2 = () => <h1>Hello, World!</h1>;
+
+//Nomeação de Components
+//PascalCase
+
+Header;
